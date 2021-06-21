5b3d2ff00e47f5bde070e5ebc6f7bfa35a955e3c
tien-duong115 <69016757+tien-duong115@users.noreply.github.com>
2021-06-20 23:31:29 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -25,20 +25,75 @@ Data folder:
 ##### song_table --> dimensional table --> one to many with fact -->  song_id PR keyUNIQUE
 RangeIndex: 71 entries, 0 to 70
 Data columns (total 5 columns):
-
+    Column  Non-Null Count  Dtype 
+---  ------  --------------  ----- 
+ 0   0       71 non-null     object
+ 1   1       71 non-null     object
+ 2   2       71 non-null     object
+ 3   3       71 non-null     int64 
+ 4   4       71 non-null     int64 
+dtypes: int64(2), object(3)
+ 
 ##### artists_table --> dimensional table  --> one to many fact --> artist_id PR keyUNIQUE 
-RangeIndex: 71 entries, 0 to 70
+RangeIndex: 69 entries, 0 to 68
 Data columns (total 5 columns):
-
+    Column  Non-Null Count  Dtype 
+---  ------  --------------  ----- 
+ 0   0       69 non-null     object
+ 1   1       69 non-null     object
+ 2   2       69 non-null     object
+ 3   3       69 non-null     object
+ 4   4       69 non-null     object
+dtypes: object(5)
 
 ##### users_table --> dimentional table --> one to many with fact --> user_id PR key UNIQUE
-RangeIndex: 8056 entries, 0 to 8055
+
+RangeIndex: 97 entries, 0 to 96
 Data columns (total 5 columns):
+    Column  Non-Null Count  Dtype 
+---  ------  --------------  ----- 
+ 0   0       97 non-null     int64 
+ 1   1       97 non-null     object
+ 2   2       97 non-null     object
+ 3   3       97 non-null     object
+ 4   4       97 non-null     object
+dtypes: int64(1), object(4)
 
 ##### time_table --> deminsional table --> one to many with fact --> start_time PR key UNIQUE
-RangeIndex: 6820 entries, 0 to 6819
+RangeIndex: 6803 entries, 0 to 6802
 Data columns (total 7 columns):
+    Column  Non-Null Count  Dtype         
+---  ------  --------------  -----         
+ 0   0       6803 non-null   datetime64[ns]
+ 1   1       6803 non-null   int64         
+ 2   2       6803 non-null   int64         
+ 3   3       6803 non-null   int64         
+ 4   4       6803 non-null   int64         
+ 5   5       6803 non-null   int64         
+ 6   6       6803 non-null   int64         
+dtypes: datetime64[ns](1), int64(6)
 
 ##### Songplay_table --> Fact table --> one to many with fact --> songplay_id PR UNIQUE 
 RangeIndex: 6820 entries, 0 to 6819
 Data columns (total 9 columns):
+    Column  Non-Null Count  Dtype         
+---  ------  --------------  -----         
+ 0   0       6820 non-null   int64         
+ 1   1       6820 non-null   datetime64[ns]
+ 2   2       6820 non-null   object        
+ 3   3       6820 non-null   object        
+ 4   4       6820 non-null   object        
+ 5   5       6820 non-null   object        
+ 6   6       6820 non-null   int64         
+ 7   7       6820 non-null   object        
+ 8   8       6820 non-null   object        
+dtypes: datetime64[ns](1), int64(2), object(6)
+
+## Repo File Overview
+`create_table.py` --> Creating tables script with defined functions in Drop, Create or Insert new table and values into Postgres db
+
+`etl.py` --> Script consist of functions that interates through file PATH folder and process the JSON files, then read the data file and inserted it into postgres db using `sql_queries.py` file
+
+`sql_queries.py` --> script contained pre-defined queries that helps in the DROP, CREATE, INSERT, and UPDATE the connected postgres db
+
+`test.ipynb` --> Use to test postgres database in checking governing data within each table
--- etl.py
+++ etl.py
@@ -6,22 +6,21 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    """ Process single file in JSON format and 
-    insert into song and artist table 
+    """Process input data (1 JSON file) and insert into songs and
+        artists tables.
 
-    * cur --> psql connection variable
-    * filepath --> file path to be processed
+    Keyword arguments:
+    * cur --        reference to connected db.
+    * filepath --   path to file to be processed.
 
-    OutPut:
-    * song_data insert into song table
-    * artist_data insert into artists table
-
+    Output:
+    * song_data in songs table.
+    * artist_data in artists table.
     """
     # open song file
-    df = pd.DataFrame(pd.read_json(filepath,
-                                        lines=True,
-                                        orient='columns'))
-
+    df = pd.DataFrame(pd.read_json( filepath,
+                                    lines=True,
+                                    orient='columns'))
     
     # insert song record
     song_data = (   df.values[0][6],
@@ -29,45 +28,44 @@ def process_song_file(cur, filepath):
                     df.values[0][1],
                     df.values[0][9],
                     df.values[0][8])
-    cur.execute(song_table_insert, song_data)    
+    cur.execute(song_table_insert, song_data)
+
     # insert artist record
     artist_data = ( df.values[0][1],
-                    df.values[0][5],
                     df.values[0][4],
+                    df.values[0][3],
                     df.values[0][2],
-                    df.values[0][3])
+                    df.values[0][2])
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
-    """ Process a single file in JSON format, filtered and converted
-    ts column into datatime data type then insert into time_df and user table  
+    """Process input data (1 JSON file) and insert into songs and
+        artists tables.
 
-    * cur --> psql connection variable 
-    
-    * filepath --> file path to be processed
+    Keyword arguments:
+    * cur --        reference to connected db.
+    * filepath --   path to file to be processed.
 
     Output:
-    * Insert into time_df table with different time granularity
-    * Insert into user_table with user data information
-    * Insert into songplay table with matching result from songselect query generated in sql_queries.py
-
-     """
-
+    * time_data in time table.
+    * user_data in users table.
+    * songplay_data in songplay table.
+    """
     # open log file
-    df = pd.DataFrame(pd.read_json(filepath,
-                                        lines=True,
-                                        orient='columns'))
-    old_df = df
-    # filter by NextSong action
-    df = df[df['page'] == 'NextSong' ]
+    df = pd.DataFrame(pd.read_json( filepath,
+                                    lines=True,
+                                    orient='columns'))
+    df_orig = df
 
+    # filter by NextSong action
+    df = df[df['page']=='NextSong']
 
     # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'], unit ='ms')
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
-    time_data =     time_data = list(zip(   t.dt.strftime('%Y-%m-%d %I:%M:%S'),
+    time_data = list(zip(   t.dt.strftime('%Y-%m-%d %I:%M:%S'),
                             t.dt.hour,
                             t.dt.day,
                             t.dt.week,
@@ -81,7 +79,6 @@ def process_log_file(cur, filepath):
                             'month',
                             'year',
                             'weekday')
-  
     time_df = pd.DataFrame( time_data,
                             columns=column_labels)
 
@@ -89,74 +86,109 @@ def process_log_file(cur, filepath):
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = old_df.get(['userId', 'firstName', 'lastName', 'gender', 'level'])
+    user_data = df_orig.get([   'userId',
+                                'firstName',
+                                'lastName',
+                                'gender',
+                                'level'])
+    # adjust column names
+    user_data.columns = [   'user_id',
+                            'first_name',
+                            'last_name',
+                            'gender',
+                            'level']
+    # remove rows with no user_id
+    user_data_clean = user_data[user_data['user_id']!= '']
+    user_data_clean = user_data_clean.dropna()
+
+    # remove duplicates
+    user_data_duplicates_removed = user_data_clean.drop_duplicates(
+                                                        'user_id',
+                                                        keep='first')
+    user_df = user_data_duplicates_removed
 
     # insert user records
     for i, row in user_df.iterrows():
-        cur.execute(user_table_insert , list(row))
+        cur.execute(user_table_insert, row)
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
+        # insert songplay record
         start_time = pd.to_datetime(
-                    row.ts,
-                    unit='ms').strftime(
-                    '%Y-%m-%d %I:%M:%S')
-        songplay_data = (start_time,
-                    row.userId,
-                    row.level,
-                    str(songid),
-                    str(artistid),
-                    row.sessionId,
-                    row.location,
-                    row.userAgent)
+                            row.ts,
+                            unit='ms').strftime(
+                            '%Y-%m-%d %I:%M:%S')
+        songplay_data = (   start_time,
+                            row.userId,
+                            row.level,
+                            str(songid),
+                            str(artistid),
+                            row.sessionId,
+                            row.location,
+                            row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
-
 def process_data(cur, conn, filepath, func):
-    """ Walk through input file path directory
+    """Walk through the whole input data directory strcture,
 
-    * cur -->  cursor, connection variable to db
-    * conn -->  connection parameter to connect 
-    * filepath --> filepath directory to walk through
-    * func --> function to be call to enumerate the datafile
+    Keyword arguments:
+    * cur --        reference to connected db.
+    * conn --       parameters (host, dbname, user, password) to
+                    connect the db.
+    * filepath --   path to file to be processed
+                    (data/song_data or data/log_data).
+    * func --       function to be called (process_song_data or
+                    process_log_data)
 
-    OutPut:
-    Within terminal console, printout the file processed
+    Output:
+    * console printouts of the data processing.
     """
-
+    # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
             all_files.append(os.path.abspath(f))
 
+    # get total number of files found
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
 
-    # iterate over  and process
+    # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
 
-
+    print('All {} files processed OK in {}'.format(num_files, filepath))
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname= user= password=")
+    """Connect to DB and call process_data (2x) to walk through
+        all the input data (data/song_data and data/log_data).
+
+#     Keyword arguments:
+#     * None
+
+#     Output:
+#     * All input data processed in DB tables.
+#     """
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user= password=")
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='your file path goes here', func=process_song_file)
-    process_data(cur, conn, filepath='your file path goes here', func=process_log_file)
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    
+
 
     conn.close()
 
